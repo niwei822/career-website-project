bcd7b2d579bfb17d3d0f446be5fc84ff4a4bc047
niwei <69645683+niwei822@users.noreply.github.com>
2023-03-05 06:01:22 +0000

    add sqlalchemy and connect to mySQL database

---
+++ database.py
@@ -0,0 +1,2 @@
+from sqlalchemy import create_engine
+engine = create_engine("mysql+pymysql://wc08zq4dr4rn2q8927du:pscale_pw_ckVRdm3iipwKf7rPD8DdNeMmhCpdza7Dd8yzh8qvOOZ@us-west.connect.psdb.cloud/hireme?charset=utf8mb4")
